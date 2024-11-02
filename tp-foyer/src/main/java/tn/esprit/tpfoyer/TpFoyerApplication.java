237fa806cc2b6b06f6962f1a08d84894f95043be
MayssaSmeti <92467037+MayssaSmeti@users.noreply.github.com>
2024-11-02 13:43:48 +0100

    Update TpFoyerApplication.java

--- tp-foyer/src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ tp-foyer/src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -1,12 +1,12 @@
 package tn.esprit.tpfoyer;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,4 +17,13 @@ public class TpFoyerApplication {
         SpringApplication.run(TpFoyerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
